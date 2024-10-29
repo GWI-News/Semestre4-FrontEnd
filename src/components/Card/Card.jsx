4c0e626fbdd17f5175908867fb880095a81f6dd9
FelipeDSilver <felipedonsilva@hotmail.com>
2024-10-29 17:17:16 -0300

    Link dos cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
-import styles from './Card.module.css';
+import styles from './NewsCard.module.css';
 
-const NewsCard = ({ noticia }) => {
-    return (
-        <Link to={`/noticia/${noticia.id}`} className={styles.link}>
-            <Card className={styles.card}>
-                <Card.Img variant="top" src={noticia.img} />
-                <Card.Body className='p-3'>
-                    <Card.Title className='m-0'>{noticia.titulo}</Card.Title>
-                </Card.Body>
-            </Card>
-        </Link>
-    );
-}
+const NewsCard = ({ noticia, categoria }) => {
+  return (
+    <Link to={`/${categoria}/${noticia.id}`} className={styles.link}>
+      <Card className={`${styles.card}`}>
+        <Card.Img variant="top" src={noticia.img} alt={noticia.alt_imagem} />
+        <Card.Body className="p-3">
+          <Card.Title className="m-0">{noticia.titulo}</Card.Title>
+        </Card.Body>
+      </Card>
+    </Link>
+  );
+};
 
 export default NewsCard;
--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,93 +1,62 @@
+// src/pages/NewsPage/NewsPage.js
 import React, { useState, useEffect } from 'react';
 import styles from './NewsPage.module.css';
 import { NavLink, useLocation } from 'react-router-dom';
-
-import { doc, getDoc, setDoc, getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection } from 'firebase/firestore';
 import { db } from '../../firebase/config';
-import { analytics } from '../../firebase/config';
-import { logEvent } from 'firebase/analytics';
-
-import NewsCard from '../../components/Card/Card';
+import NewsCard from '../../components/Card/NewsCard';
 import PesquisaTempoReal from '../../components/Pesquisa/Pesquisa';
 
 import Container from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
 
 const NewsPage = ({ categoria }) => {
-  const location = useLocation();  
+  const location = useLocation();
+  const [noticias, setNoticias] = useState([]);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  const [noticias, setNoticias] = useState([]);
   useEffect(() => {
     const getNoticias = async () => {
       const data = await getDocs(collection(db, 'Noticias'));
-      setNoticias(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
+      setNoticias(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getNoticias();
   }, []);
-  
-  useEffect(() => {
-    const buscarDados = async () => {
-      const docRef = doc(db, 'Metrics', 'categoria');
-      const docSnap = await getDoc(docRef);
-      if (!docSnap.exists()) {
-        setDoc(docRef, {});
-      }
-      if (!(categoria in docSnap.data())) {
-        setDoc(docRef, {
-          ...docSnap.data(),
-          [categoria]: 1
-        });
-      } else {
-        setDoc(docRef, {
-          ...docSnap.data(),
-          [categoria]: docSnap.data()[categoria] + 1
-        });
-      }
-    };
-    if (categoria !== undefined) {
-      buscarDados();
-    }
-    logEvent(analytics, 'Acesso_Por_Categoria', {
-      screen_name: categoria,
-    });
-  }, [categoria]);
 
   const noticiasCarrossel = noticias.slice(0, 3);
   const noticiasCards = noticias.slice(3);
 
   return (
-    <>
-      <Container fluid className={styles.newsPage}>
-        <PesquisaTempoReal />
-        <h1 className={styles.tituloPages}>Últimas Notícias</h1>
+    <Container fluid className={styles.newsPage}>
+      <PesquisaTempoReal />
+      <h1 className={styles.tituloPages}>Últimas Notícias</h1>
 
-        <Carousel indicators={false}>
-          {noticiasCarrossel.map((carouselItem, i) => (
-            <Carousel.Item key={i}>
-              <NavLink to={`/${categoria}/${carouselItem.id}&${carouselItem.title}`}>
-                <img
-                  className="d-block w-100"
-                  src={carouselItem.img}
-                  alt={carouselItem.alt_imagem}
-                />
-                <Carousel.Caption className={`${styles.carouselTexto} p-1`}>
-                  <h3 className='m-0'>{carouselItem.titulo}</h3>
-                </Carousel.Caption>
-              </NavLink>
-            </Carousel.Item>
-          ))}
-        </Carousel>
+      <Carousel indicators={false}>
+        {noticiasCarrossel.map((carouselItem, i) => (
+          <Carousel.Item key={i}>
+            <NavLink to={`/${categoria}/${carouselItem.id}`}>
+              <img
+                className="d-block w-100"
+                src={carouselItem.img}
+                alt={carouselItem.alt_imagem}
+              />
+              <Carousel.Caption className={`${styles.carouselTexto} p-1`}>
+                <h3 className="m-0">{carouselItem.titulo}</h3>
+              </Carousel.Caption>
+            </NavLink>
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
-        <div className={styles.cardContainer}>
-          {noticiasCards.map((noticia) => (
-            <NewsCard key={noticia.id} noticia={noticia} />
-          ))}
-        </div>
-      </Container>
-    </>
+      <div className={styles.cardContainer}>
+        {noticiasCards.map((noticia) => (
+          <NewsCard key={noticia.id} noticia={noticia} categoria={categoria} />
+        ))}
+      </div>
+    </Container>
   );
 };
 
